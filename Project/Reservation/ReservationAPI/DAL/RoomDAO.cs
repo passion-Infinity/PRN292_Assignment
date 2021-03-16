79d551951f119a8fbb03359e2c0161edc38ae439
Danh Nguyen <danhnlc252k@gmail.com>
2021-03-16 20:03:09 +0700

    CRUD RoomType

---
+++ Project/Reservation/ReservationAPI/DAL/RoomDAO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using ReservationAPI.DB;
+using ReservationDTO.DTO;
+
+namespace ReservationAPI.DAL
+{
+    class RoomDAO : AbstractDAO
+    {
+        public bool AddNewRoom(RoomDTO dto)
+        {
+            string sql = "spAddNewRoom";
+            string parameter = "@roomTypeID,@roomName,@price,@status,@createdDate";
+            object[] value = {dto.RoomTypeID, dto.RoomName, dto.Price, dto.Status, dto.CreatedDate};
+            return ExecuteNonQuery(sql, parameter, value);
+        }
+        public bool UpdateRoom(RoomDTO dto)
+        {
+            string sql = "spUpdateRoom";
+            string parameter = "@roomID,@roomTypeID,@roomName,@price,@status,@modifiedDate";
+            object[] value = { dto.RoomTypeID, dto.RoomName, dto.Price, dto.Status, dto.ModifiedDate };
+            return ExecuteNonQuery(sql, parameter, value);
+        }
+        public bool DeleteRoom(RoomDTO dto)
+        {
+            string sql = "spUpdateStatusRoom";
+            string parameter = "@roomID,@status,@modifiedDate";
+            object[] value = { dto.RoomTypeID, dto.Status, dto.ModifiedDate };
+            return ExecuteNonQuery(sql, parameter, value);
+        }
+    }
+}
---
+++ Project/Reservation/ReservationAPI/DAL/RoomTypeDAO.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using ReservationAPI.DB;
+using ReservationDTO.DTO;
+
+namespace ReservationAPI.DAL
+{
+    class RoomTypeDAO : AbstractDAO
+    {
+        public RoomTypeDTO GetRoomTypeByID(string id)
+        {
+            RoomTypeDTO result = null;
+            string sql = "Select RoomTypeID, RoomTypeName, People Where RoomTypeID=@roomTypeID";
+            SqlConnection conn = DBConnection.GetConnection();
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@roomTypeID", id);
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                SqlDataReader rd = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                if (rd.HasRows)
+                {
+                    if (rd.Read())
+                    {
+                        result = new RoomTypeDTO
+                        {
+                            RoomTypeID = int.Parse(rd["RoomTypeID"].ToString()),
+                            RoomTypeName = rd["RoomTypeName"].ToString(),
+                            People = int.Parse(rd["People"].ToString())
+                        };
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return result;
+        }
+        public bool AddNewRoomType(RoomTypeDTO dto)
+        {
+            string sql = "spAddNewRoomType";
+            string parameter = "@roomTypeName,@people";
+            object[] value = {dto.RoomTypeName, dto.People};
+            return ExecuteNonQuery(sql, parameter, value);
+        }
+        public bool UpdateRoomType(RoomTypeDTO dto)
+        {
+            string sql = "spUpdateRoomType";
+            string parameter = "@roomTypeID,@roomTypeName,@people";
+            object[] value = {dto.RoomTypeID, dto.RoomTypeName, dto.People};
+            return ExecuteNonQuery(sql, parameter, value);
+        }
+    }
+}
