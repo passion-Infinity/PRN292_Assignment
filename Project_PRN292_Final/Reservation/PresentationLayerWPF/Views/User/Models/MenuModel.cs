1ec2e3ab4e4e10008942ca761fbc882ccf89472e
Đào Phương Nam <ealflm@gmail.com>
2021-03-25 09:22:10 +0700

    Some thing new

---
+++ Project_PRN292_Final/Reservation/PresentationLayerWPF/Views/User/Models/MenuModel.cs
@@ -0,0 +1,6 @@
+﻿namespace PresentationLayerWPF.Views.User.Models {
+    public class MenuModel {
+        public string MenuName { get; set; }
+        public string Icon { get; set; }
+    }
+}
---
+++ Project_PRN292_Final/Reservation/PresentationLayerWPF/Views/User/ViewModels/MenuViewModels.cs
@@ -0,0 +1,34 @@
+﻿using PresentationLayerWPF.Views.User.Models;
+using System.Collections.ObjectModel;
+
+namespace PresentationLayerWPF.Views.User.ViewModels {
+    class MenuViewModels {
+        ObservableCollection<MenuModel> _menuItems { get; set; }
+
+        public ObservableCollection<MenuModel> MenuItems {
+            get { return _menuItems; }
+            set { _menuItems = value; }
+        }
+
+        public MenuViewModels() {
+            _menuItems = new ObservableCollection<MenuModel>() {
+                new MenuModel() {
+                    MenuName ="Search",
+                    Icon = "Magnify"
+                },
+                new MenuModel() {
+                    MenuName ="Cart",
+                    Icon = "Cart"
+                },
+                new MenuModel() {
+                    MenuName ="Order History",
+                    Icon = "History"
+                },
+                new MenuModel() {
+                    MenuName ="Message",
+                    Icon = "Message"
+                }
+            };
+        }
+    }
+}
